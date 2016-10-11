83918414932ff81579b9025ecfccc2b0353284c7
sestus <michail.milonakis@gmail.com>
2016-10-11 22:12:14 +0300

    add First attempt of percolation (1 faling test yet..)

--- assignment1-percolation/src/Percolation.java
+++ assignment1-percolation/src/Percolation.java
@@ -1,5 +1,92 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 /**
- * Created by mmilonakis on 11/10/2016.
+ * Created by Mike Milonakis on 11/10/2016.
  */
 public class Percolation {
+
+    private final WeightedQuickUnionUF uf;
+    private final boolean[] openSites;
+    private final int gridSize;
+    private final int topHelperSite;
+    private final int bottomHelperSite;
+
+    public Percolation(int size) {
+        if (size <= 0)
+            throw new IllegalArgumentException();
+        gridSize = size;
+        uf = new WeightedQuickUnionUF(gridSize * gridSize + 2);
+        topHelperSite = gridSize * gridSize + 1;
+        bottomHelperSite = gridSize * gridSize;
+        openTheAdditionalHelperSites();
+        openSites = new boolean[gridSize * gridSize];
+    }
+
+
+    public void open(int row, int column) {
+        validateIndices(row, column);
+        openSites[(convertTo1D(row, column))] = true;
+        unionWithNeighbors(row, column);
+    }
+
+
+    public boolean isOpen(int row, int column) {
+        validateIndices(row, column);
+        return openSites[convertTo1D(row, column)];
+    }
+
+
+    private void unionWithNeighbors(int row, int column) {
+        if ((row - 1) > 0) {
+            int upper = convertTo1D(row - 1, column);
+            if (isOpen(upper))
+                uf.connected(convertTo1D(row, column), upper);
+        }
+        if ((column + 1) <= gridSize) {
+            int right = convertTo1D(row, column + 1);
+            if (isOpen(right))
+                uf.connected(convertTo1D(row, column), right);
+        }
+        if ((row + 1) <= gridSize) {
+            int down = convertTo1D(row + 1, column);
+            if (isOpen(down))
+                uf.connected(convertTo1D(row, column), down);
+        }
+        if ((column - 1) > 0) {
+            int left = convertTo1D(row, column - 1);
+            if (isOpen(left))
+                uf.connected(convertTo1D(row, column), left);
+        }
+    }
+
+    private boolean isOpen(int index) {
+        return openSites[index];
+    }
+
+    private void validateIndices(int row, int column) {
+        if (((row <= 0) || (row > gridSize)) || ((column <= 0) || (column >
+                gridSize)))
+            throw new IndexOutOfBoundsException();
+    }
+
+    private int convertTo1D(int row, int column) {
+        return ((row - 1) * gridSize) + (column - 1);
+    }
+
+    private void openTheAdditionalHelperSites() {
+        for (int column = 1; column <= gridSize; column++) {
+            uf.union(bottomHelperSite, convertTo1D(1, column));
+            uf.union(topHelperSite, convertTo1D(gridSize, column));
+        }
+    }
+
+    public boolean isFull(int row, int column) {
+        validateIndices(row, column);
+        return isOpen(row, column) &&
+                uf.connected(convertTo1D(row, column), topHelperSite);
+    }
+
+    public boolean percolates() {
+        return uf.connected(topHelperSite, bottomHelperSite);
+    }
 }
--- assignment1-percolation/test/PercolationTest.java
+++ assignment1-percolation/test/PercolationTest.java
@@ -1,6 +1,77 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Created by Mike Milonakis on 11/10/2016.
  */
 public class PercolationTest {
 
+    private Percolation percolation;
+
+    @Before
+    public void setUp() throws Exception {
+        percolation = new Percolation(5);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void Create_NegativeGridValue_ExceptionThrown() {
+        new Percolation(-5);
+    }
+
+    @Test
+    public void Create_Grid_AllSitesBlocked() {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; i <= 5; i++) {
+                Assert.assertFalse(percolation.isOpen(i, j));
+            }
+        }
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void IsOpen_IllegalValue_ExceptionThrown() {
+        percolation.isOpen(0, 0);
+    }
+
+    @Test
+    public void Open_regularSite_BecomesOpen() {
+        percolation.open(1,1);
+
+        Assert.assertTrue(percolation.isOpen(1,1));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void IsFull_IllegalValue_Exceptionthrown() {
+        percolation.isFull(6, 6);
+    }
+
+    @Test
+    public void IsFull_SiteInBottomRowIsOpen_ReturnsTrue() {
+        percolation.open(5, 5);
+
+        Assert.assertTrue(percolation.isFull(5, 5));
+        Assert.assertFalse(percolation.isFull(5, 4));
+    }
+
+    @Test
+    public void Percolates_NoBottomSiteFull_ReturnsFalse() {
+        percolation.open(5, 5);
+        percolation.open(4, 5);
+        percolation.open(3, 5);
+        percolation.open(2, 5);
+
+        Assert.assertFalse(percolation.percolates());
+        Assert.assertFalse(percolation.isFull(1, 5));
+    }
+
+    @Test
+    public void Percolates_BottomSiteFull_ReturnsTrue() {
+        percolation.open(5, 5);
+        percolation.open(4, 5);
+        percolation.open(3, 5);
+        percolation.open(2, 5);
+        percolation.open(1, 5);
+
+        Assert.assertTrue(percolation.percolates());
+    }
 }
