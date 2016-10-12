bec4f3c395ccb0c96b89f93f071321b6e2408bb2
Mike Milonakis <michail.milonakis@gmail.com>
2016-10-12 19:54:24 +0300

    Addressed 'Backwash' issue

--- assignment1-percolation/src/Percolation.java
+++ assignment1-percolation/src/Percolation.java
@@ -5,27 +5,31 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
  */
 public class Percolation {
 
-    private final WeightedQuickUnionUF uf;
+    private WeightedQuickUnionUF uf;
     private final boolean[] openSites;
     private final int gridSize;
     private final int topHelperSite;
     private final int bottomHelperSite;
+    private final WeightedQuickUnionUF ufBackwash;
+    private boolean isPercolated;
 
     public Percolation(int size) {
         if (size <= 0)
             throw new IllegalArgumentException();
         gridSize = size;
         uf = new WeightedQuickUnionUF(gridSize * gridSize + 2);
-        topHelperSite = gridSize * gridSize + 1;
-        bottomHelperSite = gridSize * gridSize;
+        ufBackwash = new WeightedQuickUnionUF(gridSize * gridSize + 1);
+        topHelperSite = gridSize * gridSize;
+        bottomHelperSite = gridSize * gridSize + 1;
         openTheAdditionalHelperSites();
         openSites = new boolean[gridSize * gridSize];
     }
 
     private void openTheAdditionalHelperSites() {
         for (int column = 1; column <= gridSize; column++) {
-            uf.union(bottomHelperSite, convertTo1D(1, column));
-            uf.union(topHelperSite, convertTo1D(gridSize, column));
+            uf.union(bottomHelperSite, convertTo1D(gridSize, column));
+            uf.union(topHelperSite, convertTo1D(1, column));
+            ufBackwash.union(topHelperSite, convertTo1D(1, column));
         }
     }
 
@@ -47,23 +51,35 @@ public class Percolation {
     private void unionWithNeighbors(int row, int column) {
         if ((row - 1) > 0) {
             int down = convertTo1D(row - 1, column);
-            if (isOpen(down))
+            if (isOpen(down)) {
                 uf.union(convertTo1D(row, column), down);
+                ufBackwash.union(convertTo1D(row, column), down);
+            }
         }
         if ((column + 1) <= gridSize) {
             int right = convertTo1D(row, column + 1);
-            if (isOpen(right))
+            if (isOpen(right)) {
                 uf.union(convertTo1D(row, column), right);
+                ufBackwash.union(convertTo1D(row, column), right);
+            }
         }
         if ((row + 1) <= gridSize) {
             int upper = convertTo1D(row + 1, column);
-            if (isOpen(upper))
+            if (isOpen(upper)) {
                 uf.union(convertTo1D(row, column), upper);
+                ufBackwash.union(convertTo1D(row, column), upper);
+            }
         }
         if ((column - 1) > 0) {
             int left = convertTo1D(row, column - 1);
-            if (isOpen(left))
+            if (isOpen(left)) {
                 uf.union(convertTo1D(row, column), left);
+                ufBackwash.union(convertTo1D(row, column), left);
+            }
+        }
+        if (percolates()) {
+            uf = ufBackwash;
+            isPercolated = true;
         }
     }
 
@@ -83,10 +99,12 @@ public class Percolation {
     public boolean isFull(int row, int column) {
         validateIndices(row, column);
         return isOpen(row, column) &&
-                uf.connected(convertTo1D(row, column), topHelperSite);
+                ufBackwash.connected(convertTo1D(row, column), topHelperSite);
     }
 
     public boolean percolates() {
+        if (isPercolated)
+            return true;
         return uf.connected(topHelperSite, bottomHelperSite);
     }
 }
