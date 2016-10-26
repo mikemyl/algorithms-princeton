b7bb90452f9b5285a4efaa8150b83a7fd8cdd57f
Mike Milonakis <michail.milonakis@gmail.com>
2016-10-26 23:10:20 +0300

    Implement KdTree#range

--- assignment5-kdtrees/src/KdTree.java
+++ assignment5-kdtrees/src/KdTree.java
@@ -1,8 +1,10 @@
 import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.RectHV;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -24,22 +26,45 @@ public class KdTree {
 
     public void insert(Point2D point) {
         Objects.requireNonNull(point);
-        root = put(root, point, Orientation.VERTICAL);
+        put(root, point, Orientation.VERTICAL);
     }
 
-    private Node put(Node node, Point2D point, Orientation orientation) {
-        if (node == null)
-            return new Node(point);
+    private void put(Node node, Point2D point, Orientation orientation) {
+        if (root == null) {
+            root = new Node(point, new RectHV(0.0, 0.0, 1, 1));
+            return;
+        }
         Comparator<Point2D> orientationComparator =
                 getCurrentLevelComparator(orientation);
         int cmp = orientationComparator.compare(node.p, point);
-        if (cmp < 0)
-            node.lb = put(node.lb, point, orientation.next());
-        else if (cmp > 0)
-            node.rt = put(node.rt, point, orientation.next());
+        if (cmp < 0) {
+            if (node.lb == null)
+                node.lb = new Node(point, createLbRectanle(node, orientation));
+            else
+                put(node.lb, point, orientation.next());
+        }
+        else {
+            if (node.p.compareTo(point) == 0)
+                return;
+            if (node.rt == null)
+                node.rt = new Node(point, createRtRectanle(node, orientation));
+            else
+                put(node.rt, point, orientation.next());
+        }
+    }
+
+    private RectHV createLbRectanle(Node node, Orientation orientation) {
+        if (orientation == Orientation.VERTICAL)
+            return new RectHV(node.rect.xmin(), node.rect.ymin(), node.p.x(), node.rect.ymax());
         else
-            node.p = point;
-        return node;
+            return new RectHV(node.rect.xmin(), node.rect.ymin(), node.rect.xmax(), node.p.y());
+    }
+
+    private RectHV createRtRectanle(Node node, Orientation orientation) {
+        if (orientation == Orientation.VERTICAL)
+            return new RectHV(node.p.x(), node.rect.ymin(), node.rect.xmax(), node.rect.ymax());
+        else
+            return new RectHV(node.rect.xmin(), node.p.y(), node.rect.xmax(), node.rect.ymax());
     }
 
     public boolean contains(Point2D point2D) {
@@ -65,25 +90,45 @@ public class KdTree {
                 Point2D.Y_ORDER : Point2D.X_ORDER;
     }
 
-    public Iterable<Point2D> range(RectHV rectHV) {
-        return range(root, rectHV, Orientation.VERTICAL, new ArrayList<Point2D>());
-    }
 
-    private Iterable<Point2D> range(Node node, RectHV rectHV, Orientation orientation, ArrayList<Point2D> rangeList) {
-        if (node != null) {
-
-
+    public Iterable<Point2D> range(RectHV rect) {
+        List<Point2D> pointList = new ArrayList<>();
+        Queue<Node> nodeQueue = new Queue<>();
+        nodeQueue.enqueue(root);
+        int nodesInCurrentLevel = 1;
+        int nodesInNextLevel = 0;
+        Orientation orientation = Orientation.VERTICAL;
+        while (!nodeQueue.isEmpty()) {
+            Node node = nodeQueue.dequeue();
+            nodesInCurrentLevel--;
+            if (rect.contains(node.p))
+                pointList.add(node.p);
+            if ((node.lb != null ) && (node.lb.rect.intersects(rect))) {
+                nodeQueue.enqueue(node.lb);
+                nodesInNextLevel++;
+            }
+            if ((node.rt != null ) && (node.rt.rect.intersects(rect))) {
+                nodeQueue.enqueue(node.rt);
+                nodesInNextLevel++;
+            }
+            if (nodesInCurrentLevel == 0) {
+                nodesInCurrentLevel = nodesInNextLevel;
+                nodesInNextLevel = 0;
+                orientation = orientation.next();
+            }
         }
+        return pointList;
     }
 
-    private static class Node {
+        private static class Node {
         private Point2D p;
         private RectHV rect;
         private Node lb;
         private Node rt;
 
-        Node(Point2D point) {
+        Node(Point2D point, RectHV rect) {
             this.p = point;
+            this.rect = rect;
         }
     }
 
